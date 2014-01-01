bda690a84123f99898a5936ec9645181424c2a1e
Zane Sterling <sterling.zane@gmail.com>
2013-12-31 21:56:22 -0500

    Add class structure documentation

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-	return render_template("home.html")
+def index():
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	app.debug = True
