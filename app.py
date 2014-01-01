f987726382b5d8666ca7c8caceb2112b9d1a3683
Zane Sterling <sterling.zane@gmail.com>
2013-12-31 20:23:07 -0500

    Add the logo! ^_^

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "<h1> Homepage! </h1>"
+	return render_template("home.html")
 
 if __name__ == "__main__":
 	app.debug = True
