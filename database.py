f0c563a461a58dad43415edce7a35eb521fc97fa
Christopher Burke <cburke496@gmail.com>
2014-01-09 18:55:06 -0500

    Split each function into three

--- database.py
+++ database.py
@@ -4,44 +4,62 @@ userFields = ["username","token", "tags", "friends", "followed"]
 tagFields = ["id", "name", "description", "color", "creator", "bookmrks", "privacy"]
 bookmarks = ["id", "link", "tags", "title"]
 
-#The type is either "users", "tags", or "bookmarks"
-#The id is the username or id you want
-def getInfo(type,id):
+
+def getUser(username):
     connection = sqlite3.connect('marx.db')
-    if type == "users":
-        q = "select * from users where username=?"
-        cursor = connection.execute(q, [id])
-    else:
-        q = "select * from ? where id=?"
-        cursor = connection.execute(q, [type,id])
+    q = "select * from users where username=?"
+    cursor = connection.execute(q, [username])
     results = [line for line in cursor]
     return results
 
 
-#The type is either "users", "tags", or "bookmarks"
-#The id is the username or id you want
-#The info is a list of the info you want to store
-def setInfo(type,id,info):
+def getTag(id):
     connection = sqlite3.connect('marx.db')
-    if type == "users":
-        fields = userfields
-        numFields = 5
-        idName = "username"
-    elif type == "tags":
-        q = "select * from tags where id=?"
-        fields = tagfields
-        numFields = 7
-        idName = "id"
-    elif type == "bookmarks":
-        q = "select * from bookmarks where id=?"
-        fields = bookmarkfields
-        numFields = 4
-        idName = "id"
-    q = "select * from ? where ?=?"
-    cursor = connection.execute(q, [type,idName,id])
+    q = "select * from tags where id=?"
+    cursor = connection.execute(q, [id])
     results = [line for line in cursor]
-    for i in range(0,numFields):
-            if results[i] != info[i]:
-                q = "update users set ?=? where ?=?"
-                cursor = connection.execute(q, [fields[i],info[i],idName,id])
+    return results
+
+
+def getBookmark(id):
+    connection = sqlite3.connect('marx.db')
+    q = "select * from bookmark where id=?"
+    cursor = connection.execute(q, [id])
+    results = [line for line in cursor]
+    return results
+
+
+def setUser(username, info):
+    connection = sqlite3.connect('marx.db')
+    q = "select * from users where username=?"
+    cursor = connection.execute(q, [username])
+    results = [line for line in cursor]
+    for i in range(0,5):
+        if results[i] != info[i]:
+            q = "update users set ?=? where username=?"
+            cursor = connection.execute(q, [userfields[i],info[i],username])
+    connection.commit()
+
+
+def setTag(id, info):
+    connection = sqlite3.connect('marx.db')
+    q = "select * from tags where id=?"
+    cursor = connection.execute(q, [id])
+    results = [line for line in cursor]
+    for i in range(0,7):
+        if results[i] != info[i]:
+            q = "update tags set ?=? where id=?"
+            cursor = connection.execute(q, [tagfields[i],info[i],id])
+    connection.commit()
+
+
+def setBookmark(id, info):
+    connection = sqlite3.connect('marx.db')
+    q = "select * from bookmarks where id=?"
+    cursor = connection.execute(q, [id])
+    results = [line for line in cursor]
+    for i in range(0,4):
+        if results[i] != info[i]:
+            q = "update bookmarks set ?=? where id=?"
+            cursor = connection.execute(q, [bookmarkfields[i],info[i],id])
     connection.commit()
