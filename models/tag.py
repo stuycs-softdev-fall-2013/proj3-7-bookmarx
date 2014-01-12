1bcced14890b843b12ce864bc9b67ab051485973
cburke496 <cburke496@gmail.com>
2014-01-12 16:57:13 -0500

    Overrode load and unload, but cant figure out error

--- models/tag.py
+++ models/tag.py
@@ -1,11 +1,34 @@
 from model import Model
+import database
 
 class Tag(Model):
+  idnum = 0
   name = "Default Tag Name"
   description = "Default Tag Description"
   color = [0, 0, 0] # Store color as RGB list, I guess. Probably FIXME
   creator = None # User who created the tag
   bookmarks = [] # Bookmarks with this tag
   privacy = "private" # We probably want something more like an enum here, FIXME
+  def __init__(self, idnumber):
+    self.idnum = idnumber
+    super(Tag, self).__init__()
   def __repr__(self):
     return "<Tag %d>"%(self.id)
+  def load(self):
+    variables = database.getTag(self.name)
+    if variables != 0:
+      self.name = variables[1]
+      self.description = variables[2]
+      self.color = variables[3].split(',')
+      self.creator = variables[4]
+      self.bookmarks = variables[5].split(',')
+      self.privacy = variables[6]
+      print self.name
+      print self.description
+      print self.color
+      print self.creator
+      print self.bookmarks
+      print self.privacy
+  def unload(self):
+    variables = [self.idnum,self.name,self.description,','.join(self.color),self.creator,','.join(self.bookmarks),self.privacy]
+    database.setTag(variables)
