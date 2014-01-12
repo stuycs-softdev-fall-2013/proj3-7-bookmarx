47827eac4c96032fab99b1a5447d91814024b0e9
Christopher Burke <cburke496@gmail.com>
2014-01-12 16:31:51 -0500

    Overrode load and unload

--- models/user.py
+++ models/user.py
@@ -1,14 +1,28 @@
 from model import Model
+import database
 
 class User(Model):
   name = "Default Username"
+  token = ""
   tags = []
+  friends = []
+  followed = []
   def __init__(self, username):
-    name = username
+    self.name = username
     super(User, self).__init__()
   def __repr__(self):
     return "<User %d>"%(self.id)
   def load(self):
-    pass
+    variables = database.getUser(self.name)
+    if variables != 0:
+      self.token = variables[1]
+      self.tags = variables[2].split(',')
+      self.friends = variables[3].split(',')
+      self.followed = variables[4].split(',')
+      print self.token
+      print self.tags
+      print self.friends
+      print self.followed
   def unload(self):
-    pass
+    variables = [self.name,self.token,",".join(self.tags),",".join(self.friends),",".join(self.followed)]
+    database.setUser(variables)
