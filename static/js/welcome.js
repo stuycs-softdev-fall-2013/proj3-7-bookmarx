aef8a3ab9fe4b57d12f2146c78f7e691b0f340a7
Zane Sterling <sterling.zane@gmail.com>
2014-01-12 15:08:21 -0500

    Add Facebook login.
    I should figure out how to get the good-looking button.

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,16 +1,20 @@
-var count = 0;
+var gCount = 0;
+var fCount = 0;
+setTimeout(function() {
+	fCount = 1;
+}, 5000);
 var signinCallback = function(authResult) {
-	if (count != 0) {
-		if (authResult['status']['signed_in']) {
-			token = authResult.access_token;
-			$.ajax({
-				url: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token,
-				data: null,
-				dataType: "jsonp"  
-			}).success(getUserInfo);
-		}
-	} else {
-		count++;
+	if (gCount == 0) {
+		gCount++;
+		return;
+	}
+	if (authResult['status']['signed_in']) {
+		token = authResult.access_token;
+		$.ajax({
+			url: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token,
+			data: null,
+			dataType: "jsonp"  
+		}).success(getUserInfo);
 	}
 }
 
@@ -26,6 +30,32 @@ var redirectUser = function(user) {
 	var user_id = user.id;
 	$().redirect(URL + 'login', {
 		usern : 'PLACEHOLDER', // TODO add usern handling (requires db)
-		user_id : user_id
+		user_id : "g" + user_id
 	});
 }
+
+window.fbAsyncInit = function() {
+	FB.init({
+		appId	: '511674402281522',
+		status	: true, // check login status
+		cookie	: true, // enable cookies to allow the server to access the session
+		xfbml	: true	// parse XFBML
+	});
+
+	// Triggered when there is a change in user status, eg. login, logout
+	FB.Event.subscribe('auth.authResponseChange', function(response) {
+		console.log(fCount);
+		console.log(response);
+		if (fCount == 0) {
+			return;
+		}
+		if (response.status === 'connected') {
+			$().redirect(URL + 'login', {
+				usern : 'PLACEHOLDER', // TODO add usern handling (requires db)
+				user_id : "f" + response.authResponse.userID
+			});
+		} else {
+			console.log("wat response.status was not connected, facebook");
+		}
+	});
+};
