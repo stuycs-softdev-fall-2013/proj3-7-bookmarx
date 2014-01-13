14792c0dcc54748ed206a4a6159e6f5ce4a98309
Zane Sterling <sterling.zane@gmail.com>
2014-01-12 21:39:34 -0500

    Clean up main directory.

--- bookmark.py
+++
@@ -1,8 +0,0 @@
-class Bookmark:
-	def __init__(self, name, link):
-		self.name = name
-		self.link = link
-		self.tags = []
-	def __del__(self):
-		#save self to database
-		pass
--- tag.py
+++
@@ -1,7 +0,0 @@
-class Tag:
-	def __init__(self, name):
-		self.name = name
-		self.color = 0x0000ff
-	def __del__(self):
-		#save self to database
-		pass
