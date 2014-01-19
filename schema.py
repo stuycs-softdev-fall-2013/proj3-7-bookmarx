0ee37b52b8871de1dd65d0b2cf1b8d323fa2d116
Zane Sterling <sterling.zane@gmail.com>
2014-01-19 14:05:22 -0500

    Make schema easier to edit, add table-dropping

--- schema.py
+++ schema.py
@@ -2,19 +2,22 @@ import sqlite3
 
 
 connection = sqlite3.connect('marx.db')
-q1 = "CREATE TABLE users(username TEXT, token TEXT)"
-q2 = "CREATE TABLE tags(id INTEGER, name TEXT, description TEXT, color TEXT, creator TEXT, privacy TEXT)"
-q3 = "CREATE TABLE bookmarks(id INTEGER, link TEXT, title TEXT)"
-q4 = "CREATE TABLE taggings(bookmark INTEGER, tag INTEGER)"
-q5 = "CREATE TABLE friendships(user1 TEXT, user2 TEXT)"
-q6 = "CREATE TABLE followings(user TEXT, tag TEXT)"
+queries = [
+ "DROP TABLE IF EXISTS users",
+ "DROP TABLE IF EXISTS tags",
+ "DROP TABLE IF EXISTS bookmarks",
+ "DROP TABLE IF EXISTS taggings",
+ "DROP TABLE IF EXISTS friendships",
+ "DROP TABLE IF EXISTS followings",
+ "CREATE TABLE users(username TEXT, token TEXT)",
+ "CREATE TABLE tags(id INTEGER, name TEXT, description TEXT, color TEXT, creator TEXT, privacy TEXT)",
+ "CREATE TABLE bookmarks(id INTEGER, link TEXT, title TEXT)",
+ "CREATE TABLE taggings(bookmark INTEGER, tag INTEGER)",
+ "CREATE TABLE friendships(user1 TEXT, user2 TEXT)",
+ "CREATE TABLE followings(user TEXT, tag TEXT)"
+]
 
-cursor = connection.execute(q1)
-cursor = connection.execute(q2)
-cursor = connection.execute(q3)
-cursor = connection.execute(q4)
-cursor = connection.execute(q5)
-cursor = connection.execute(q6)
-
+for q in queries:
+    connection.execute(q)
 
 connection.commit()
