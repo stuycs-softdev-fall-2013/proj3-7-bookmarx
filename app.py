af37ddfefa568e063fbe945efa9ab1380684080a
Zane Sterling <sterling.zane@gmail.com>
2014-01-19 14:40:44 -0500

    Add user registration

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import render_template, url_for, request, session, redirect
-from models import user, tag, bookmark
 from models.user import User
 from models.tag import Tag
 from models.bookmark import Bookmark
@@ -10,23 +9,20 @@ app.secret_key = "wacky potato fingers"
 
 @app.route("/welcome")
 def welcome():
-	if 'usern' in session:
+	if 'user_id' in session:
 		return redirect(url_for('home'))
 	return render_template("welcome.html")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-	if 'usern' not in session:
+	if 'user_id' not in session:
 	    return redirect(url_for("welcome"))
 
 	d = {}
-	d['user'] = user.User(session['usern'])
-	t = tag.Tag()
+	d['user'] = User(session['user_id'])
+	t = Tag("Search engines")
 	d['user'].tags.append(t)
-	b = bookmark.Bookmark()
-	b.link = "http://google.com"
-	b.title = "Googggggggle"
-	t.bookmarks.append(b)
+	b = Bookmark("http://google.com", "Googggggggle")
 	b.tags.append(t)
 	t.unload()
 	b.unload()
@@ -34,20 +30,33 @@ def home():
 
 @app.route("/login", methods=["POST"])
 def login():
-	# TODO add usern / id verification (requires db)
-	if 'usern' not in request.form or 'user_id' not in request.form:
+	# TODO add id verification
+	if 'user_id' not in request.form:
 		return redirect(url_for('welcome'))
 
-	session['usern'] = request.form['usern']
 	session['user_id'] = request.form['user_id']
-	return redirect(url_for('home'))
+	user = User(session['user_id'])
+	if user.username:
+	    return redirect(url_for('home'))
+	else:
+	    return redirect(url_for('register'))
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if 'user_id' not in session:
+        return redirect(url_for('welcome'))
+    if request.method == "GET":
+        return render_template("register.html")
+
+    user = User(session['user_id'])
+    user.username = request.form['usern']
+    user.unload()
+    return redirect(url_for('home'))
 
 @app.route("/logout")
 def logout():
-	if 'usern' in session:
-		del session['usern']
-	if 'pswd' in session:
-		del session['pswd']
+	if 'user_id' in session:
+		del session['user_id']
 	return redirect(url_for('home'))
 
 if __name__ == "__main__":
