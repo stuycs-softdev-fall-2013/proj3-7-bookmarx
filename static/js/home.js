f38ef48b3cca84c0eb7c42394d76790c05f12e8b
Zane Sterling <sterling.zane@gmail.com>
2014-01-20 12:22:36 -0500

    Clean up home.js

--- static/js/home.js
+++ static/js/home.js
@@ -3,32 +3,10 @@ $(function() {
 	var sortables = $(".sortable")
 	    .sortable()
         .disableSelection();
-    var formDropped = false;
 
-	var makeBookmark = function() {
-	    if (formDropped) {
-		    removeBookmarkMaker();
-		} else {
-		    var form = $('<form id="bookmark-maker"></form>')
-		        .hide()
-		        .append($('<input placeholder="Title"></input>'))
-		        .append($('<br>'))
-		        .append($('<input placeholder="Link"></input>'))
-		        .append($('<br>'))
-		        .append($('<a href="#" class="btn btn-success btn-xs">Create</a>'))
-		        .insertAfter('#make-bookmark')
-		        .slideDown();
-		    ;
-		    formDropped = true;
-		}
-		// TODO: add ajax call to add bookmark to server
+	var toggleBookmarkForm = function() {
+        $('#bookmark-form').slideToggle();
 	}
 
-	var removeBookmarkMaker = function() {
-	    var maker = $('#bookmark-maker');
-		maker.slideUp(maker.remove);
-		formDropped = false;
-	}
-
-	$("#make-bookmark").click(makeBookmark);
+	$("#make-bookmark").click(toggleBookmarkForm);
 });
