f39bddda38694b2f0e3bfee9deac37b9d699ce5f
Zane Sterling <sterling.zane@gmail.com>
2014-01-20 12:12:47 -0500

    Add sliding to bookmark maker.

--- static/js/home.js
+++ static/js/home.js
@@ -3,24 +3,31 @@ $(function() {
 	var sortables = $(".sortable")
 	    .sortable()
         .disableSelection();
-    var formDropped = 0;
+    var formDropped = false;
 
 	var makeBookmark = function() {
-	    if (formDropped != 1) {
+	    if (formDropped) {
+		    removeBookmarkMaker();
+		} else {
 		    var form = $('<form id="bookmark-maker"></form>')
+		        .hide()
 		        .append($('<input placeholder="Title"></input>'))
 		        .append($('<br>'))
 		        .append($('<input placeholder="Link"></input>'))
 		        .append($('<br>'))
-		        .append($('<input type="submit"></input>'))
+		        .append($('<a href="#" class="btn btn-success btn-xs">Create</a>'))
 		        .insertAfter('#make-bookmark')
+		        .slideDown();
 		    ;
-		    formDropped = 1;
-		} else {
-		    $('#bookmark-maker').remove();
-		    formDropped = 0;
+		    formDropped = true;
 		}
 		// TODO: add ajax call to add bookmark to server
+	}
+
+	var removeBookmarkMaker = function() {
+	    var maker = $('#bookmark-maker');
+		maker.slideUp(maker.remove);
+		formDropped = false;
 	}
 
 	$("#make-bookmark").click(makeBookmark);
