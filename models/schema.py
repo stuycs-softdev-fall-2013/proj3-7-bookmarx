95322dc00ae64d7c6ffeab8f05cbaf52aad5faa6
cburke496 <cburke496@gmail.com>
2014-01-20 19:19:15 -0500

    Moved schema.py to the models folder where the database is

---
+++ models/schema.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+
+connection = sqlite3.connect('marx.db')
+queries = [
+ "DROP TABLE IF EXISTS users",
+ "DROP TABLE IF EXISTS tags",
+ "DROP TABLE IF EXISTS bookmarks",
+ "DROP TABLE IF EXISTS taggings",
+ "DROP TABLE IF EXISTS friendships",
+ "DROP TABLE IF EXISTS followings",
+ "CREATE TABLE users(username TEXT, user_id TEXT)",
+ "CREATE TABLE tags(id INTEGER, name TEXT, description TEXT, color TEXT, creator TEXT, privacy TEXT)",
+ "CREATE TABLE bookmarks(id INTEGER, link TEXT, title TEXT)",
+ "CREATE TABLE taggings(bookmark INTEGER, tag INTEGER)",
+ "CREATE TABLE friendships(user1 TEXT, user2 TEXT)",
+ "CREATE TABLE followings(user TEXT, tag TEXT)"
+]
+
+for q in queries:
+    connection.execute(q)
+
+connection.commit()
